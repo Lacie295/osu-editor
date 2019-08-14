8ee56a425f811bafce191c237ff98d8ada36f788
mupfel <wolframelp@gmail.com>
2019-08-14 02:47:37 +0200

    legacy timingpoint tests

--- src/main/scala/components/Inherited_legacy.scala
+++ src/main/scala/components/Inherited_legacy.scala
@@ -2,10 +2,19 @@ package components
 
 import utils.TimeStamp
 
-class Inherited_legacy(t: TimeStamp, multi: Float, ss: Int, si: Int, vol: Int, ki: Boolean) extends TimingPoint_legacy(t, ss, si, vol, ki){
-  private var _svMultiplier: Float = multi
+class Inherited_legacy(t: TimeStamp, multi: Double, ss: Int, si: Int, vol: Int, ki: Boolean) extends TimingPoint_legacy(t, ss, si, vol, ki){
+  private var _svMultiplier: Double = multi
 
-  def svMultiplier: Float = _svMultiplier
+  def svMultiplier: Double = _svMultiplier
 
-  def svMultiplier_=(multi: Float): Unit = _svMultiplier = multi
+  def svMultiplier_=(multi: Double): Unit = _svMultiplier = multi
+
+  def canEqual(a: Any): Boolean = a.isInstanceOf[Inherited_legacy]
+
+  override def equals(that: Any): Boolean = {
+    that match {
+      case that: Inherited_legacy => that.canEqual(this) && this.time == that.time && this.svMultiplier == that.svMultiplier && this.sampleIndex == that.sampleIndex && this.sampleSet == that.sampleSet && this.volume == that.volume && this.kiai == that.kiai
+      case _ => false
+    }
+  }
 }
