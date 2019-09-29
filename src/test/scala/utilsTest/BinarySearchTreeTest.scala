a71c97b7f74e6acac38262b585336515f1d3f43f
Lazyboy007 <sami.bosch@gmail.com>
2019-09-29 17:51:52 +0200

    Working bst deletion

--- src/test/scala/utilsTest/BinarySearchTreeTest.scala
+++ src/test/scala/utilsTest/BinarySearchTreeTest.scala
@@ -10,9 +10,13 @@ class BinarySearchTreeTest extends BaseTest {
     val bst: BinarySearchTree[Int, Int] = BinarySearchTree[Int, Int]()
 
     bst.insert(1, 1)
+    assert(bst.valid)
     bst.insert(4, 5)
+    assert(bst.valid)
     bst.insert(3, 2)
+    assert(bst.valid)
     bst.insert(0, 1)
+    assert(bst.valid)
 
     assert(bst(1) == List(1))
     assert(bst(4) == List(5))
@@ -21,10 +25,13 @@ class BinarySearchTreeTest extends BaseTest {
     assert(bst(2) == bst(1))
 
     bst.insert(1, 2)
+    assert(bst.valid)
     assert(bst.delete(4) == List(5))
+    assert(bst.valid)
     assert(bst(1) == List(1, 2))
     assert(bst(4) == bst(3))
-    assert(bst.delete(1, 1) == 1)
+    assert(bst.delete(1, 1) == List(1))
+    assert(bst.valid)
     assert(bst(1) == List(2))
   }
 
@@ -32,10 +39,15 @@ class BinarySearchTreeTest extends BaseTest {
     val bst: BinarySearchTree[Int, Int] = BinarySearchTree[Int, Int]()
 
     bst.insert(1, 1)
+    assert(bst.valid)
     bst.insert(1, 6)
+    assert(bst.valid)
     bst.insert(4, 5)
+    assert(bst.valid)
     bst.insert(3, 2)
+    assert(bst.valid)
     bst.insert(0, 1)
+    assert(bst.valid)
 
     var l: ListBuffer[Int] = new ListBuffer[Int]
 
